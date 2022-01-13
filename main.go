1c18fb8787d78d626e402d35107d4b2219919d79
Arthur She <arthur@aihavefun.com>
2022-01-13 17:43:15 +0800

    feat: modify api title and description with swagger

--- main.go
+++ main.go
@@ -1,6 +1,16 @@
+// Package classification Member system API.
+//
+// The purpose of this application is to provide an application
+// that is using plain go code to define an API
+//
+// This should demonstrate all the possible comment annotations
+// that are available to turn go code into a fully compliant swagger 2.0 spec
+//
 // Schemes: http
 // Host: localhost
 // Version: 0.0.1
+// Consumes: application/json
+// Produces: application/json
 // swagger:meta
 package main
 
